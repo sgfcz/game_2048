95bed17f5340cfff1bb2769a9fdb5183df81e683
sgfcz <coderxiao@sohu.com>
2022-09-22 18:23:58 +0800

    add mainwindow function

--- QTCPP2048/QTCPP2048/MainWindow.cpp
+++ QTCPP2048/QTCPP2048/MainWindow.cpp
@@ -1,1 +1,78 @@
 #include "MainWindow.h"
+#include <iostream>
+
+MainWindow::MainWindow(QWidget* parent)
+{
+	ui.setupUi(this);
+	ui.PreviousButton->setFocusPolicy(Qt::FocusPolicy::NoFocus);
+	ui.NewGameButton->setFocusPolicy(Qt::FocusPolicy::NoFocus);
+
+	start();
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::start()
+{
+	//TODO 初始化游戏，计分
+	_start = true;
+	Block *b1 = new Block(2, QColor(120, 120, 120));
+	ui.block_0->addWidget(b1);
+}
+
+void MainWindow::end()
+{
+	//TODO 判断游戏是否结束
+}
+
+void MainWindow::moveLeft()
+{
+	//TODO 向左移动逻辑
+	std::cout << "Move Left!" << std::endl;
+	end();
+}
+
+void MainWindow::moveRight()
+{
+	//TODO 向右移动逻辑
+	std::cout << "Move Right!"<< std::endl;
+	end();
+}
+
+void MainWindow::moveUp()
+{
+	//TODO 向上移动逻辑
+	std::cout << "Move Up!"<< std::endl;
+	end();
+}
+
+void MainWindow::movedown()
+{
+	//TODO 向下移动逻辑
+	//作业
+	std::cout << "Move Down!"<< std::endl;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent* event)
+{
+	switch (event->key()) {
+	case Qt::Key_Left:
+		moveLeft();
+		break;
+	case Qt::Key_Right:
+		moveRight();
+		break;
+	case Qt::Key_Up:
+		moveUp();
+		break;
+	case Qt::Key_Down:
+		movedown();
+		break;
+	default:
+		break;
+	}
+}
+
--- QTCPP2048/QTCPP2048/MainWindow.h
+++ QTCPP2048/QTCPP2048/MainWindow.h
@@ -1,8 +1,29 @@
 #pragma once
+#include <QtWidgets>
+#include <QtWidgets/QMainWindow>
+#include <QKeyEvent>
 #include "ui_MainWindow.h"
+#include "Block.h"
 
-
-class MainWindow
+class MainWindow : public QMainWindow
 {
+	Q_OBJECT
+public:
+	MainWindow(QWidget* parent = Q_NULLPTR);
+	~MainWindow();
+
+private:
+	Ui::MainWindow ui;
+	bool _start = false;
+
+private:
+	void start();
+	void end();
+	void moveLeft();
+	void moveRight();
+	void moveUp();
+	void movedown();
+
+	void keyPressEvent(QKeyEvent* event);
 };
 
