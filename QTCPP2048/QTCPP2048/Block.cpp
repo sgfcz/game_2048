b44ddcfc820305560f75df3b1fcdf63d3af53696
sgfcz <coderxiao@outlook.com>
2022-09-23 20:12:05 +0800

    1.使用自定义类来读取颜色， 2.使用setstylesheet

--- QTCPP2048/QTCPP2048/Block.cpp
+++ QTCPP2048/QTCPP2048/Block.cpp
@@ -2,6 +2,7 @@
 
 Block::Block(int number, QWidget *parent) : QLabel(parent)
 {
+	this->setAutoFillBackground(true);
 	BlockSetColor(number);
 	BlockSetFont(number);
 }
@@ -12,29 +13,39 @@ Block::~Block()
 
 void Block::SetBlock(int number)
 {
-	palette.setColor(QPalette::Background, BlockGetColor(number));
+	QPalette palette = this->palette();
+	this->setStyleSheet(QString("background-color: %1").arg(QString::fromStdString(BlockGetColor(number))));
+	if (number == 2 || number == 4)
+		palette.setColor(QPalette::WindowText, QColor(0x776e65));
+	else
+		palette.setColor(QPalette::WindowText, QColor(0xf9f6f2));
+	this->setPalette(palette);
 	this->setText(QString::number(number));
 }
 
 void Block::BlockSetColor(int number)
 {
-	palette.setColor(QPalette::Background, BlockGetColor(number));
+	QPalette palette;
 	if (number == 2 || number == 4)
 		palette.setColor(QPalette::WindowText, QColor(0x776e65));
 	else 
 		palette.setColor(QPalette::WindowText, QColor(0xf9f6f2));
-	this->setAutoFillBackground(true);
+	this->setStyleSheet(QString("background-color: %1").arg(QString::fromStdString(BlockGetColor(number))));
+	//palette的优先级小于setStyleSheet，只要父窗口设置了此函数，其他palette就会失效
 	this->setPalette(palette);
 }
 
 void Block::BlockSetFont(int number)
 {
 	this->setText(QString::number(number));
-	this->setFont(QFont("Arial", 30, true));
+	this->setFont(QFont("Arial", 30, 75));
 	this->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 }
 
-QColor Block::BlockGetColor(int number)
+std::string Block::BlockGetColor(int number)
 {
-	return ColorMap[number];
+	for (BlockColor bc : _colorMap) {
+		if (bc.getnumber() == number)
+			return bc.getcolor();
+	}
 }
--- QTCPP2048/QTCPP2048/Block.h
+++ QTCPP2048/QTCPP2048/Block.h
@@ -1,6 +1,21 @@
 #pragma once
 #include <QLabel>
-#include <map>
+#include <vector>
+class BlockColor
+{
+public:
+	BlockColor(int number, std::string color) : cnumber(number), ccolor(color) {
+	}
+	int getnumber() {
+		return cnumber;
+	}
+	std::string getcolor() {
+		return ccolor;
+	}
+private:
+	int cnumber;
+	std::string ccolor;
+};
 
 class Block : public QLabel
 {
@@ -12,16 +27,14 @@ public:
 	void SetBlock(int number);
 private:
 	int number = 0;
-	std::map<int, QColor> ColorMap{ 
-		{2, QColor(0xeee4da)}, {4, QColor(0xede0c8)}, {8, QColor(0xf2b179)},
-		{16, QColor(0xf59563)}, {32, QColor(0xf67c5f)}, {64, QColor(0xf65e3b)},
-		{128, QColor(0xedcf72)}, {256, QColor(0xedcc61)}, {512, QColor(0xedc850)},
-		{1024, QColor(0xedc53f)}, {2048, QColor(0xedc22e)}};
+	std::vector<BlockColor> _colorMap{ 
+		{BlockColor(2, "#eee4da")}, {BlockColor(4, "#ede0c8")}, {BlockColor(8, "#f2b179")},
+		{BlockColor(16, "#f59563")}, {BlockColor(32, "#f67c5f")}, {BlockColor(64, "#f65e3b")},
+		{BlockColor(128, "#edcf72")}, {BlockColor(256, "#edcc61")}, {BlockColor(512, "#edc850")},
+		{BlockColor(1024, "#edc53f")}, {BlockColor(2048, "#edc22e")}};
 
 private:
 	void BlockSetColor(int number);
 	void BlockSetFont(int number);
-	QColor BlockGetColor(int number);
-	QPalette palette;
+	std::string BlockGetColor(int number);
 };
-
