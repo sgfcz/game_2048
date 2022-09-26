f8c2a19948762ae965caae83e24b6639ff2d1040
sgfcz <coderxiao@outlook.com>
2022-09-26 17:58:43 +0800

    change main

--- QtCpp2048/main.cpp
+++ QtCpp2048/main.cpp
@@ -1,10 +1,12 @@
-#include <QApplication>
-#include <QPushButton>
+#include <QtWidgets/QApplication>
+#include "ui/MainWindow.h"
 
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+int main(int argc, char* argv[])
+{
+	QApplication a(argc, argv);
+	
+	MainWindow w;
+	w.show();
+	w.start();
+	return a.exec();
 }
