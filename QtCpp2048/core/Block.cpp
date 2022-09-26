bbe221ccca9270b33b030226468626a546d1cdb0
sgfcz <coderxiao@outlook.com>
2022-09-26 18:18:07 +0800

    fix number to sendNumber

--- QtCpp2048/core/Block.cpp
+++ QtCpp2048/core/Block.cpp
@@ -1,50 +1,50 @@
 #include "Block.h"
 
-Block::Block(int number, QWidget *parent) : QLabel(parent)
+Block::Block(int moveNumber, QWidget *parent) : QLabel(parent)
 {
 	this->setAutoFillBackground(true);
-	BlockSetColor(number);
-	BlockSetFont(number);
+	BlockSetColor(moveNumber);
+	BlockSetFont(moveNumber);
 }
 
 Block::~Block()
 = default;
 
-void Block::SetBlock(int number)
+void Block::SetBlock(int moveNumber)
 {
 	QPalette palette = this->palette();
-	this->setStyleSheet(QString("background-color: %1").arg(QString::fromStdString(BlockGetColor(number))));
-	if (number == 2 || number == 4)
+	this->setStyleSheet(QString("background-color: %1").arg(QString::fromStdString(BlockGetColor(moveNumber))));
+	if (moveNumber == 2 || moveNumber == 4)
 		palette.setColor(QPalette::WindowText, QColor(0x776e65));
 	else
 		palette.setColor(QPalette::WindowText, QColor(0xf9f6f2));
 	this->setPalette(palette);
-	this->setText(QString::number(number));
+	this->setText(QString::number(moveNumber));
 }
 
-void Block::BlockSetColor(int number)
+void Block::BlockSetColor(int moveNumber)
 {
 	QPalette palette;
-	if (number == 2 || number == 4)
+	if (moveNumber == 2 || moveNumber == 4)
 		palette.setColor(QPalette::WindowText, QColor(0x776e65));
 	else 
 		palette.setColor(QPalette::WindowText, QColor(0xf9f6f2));
-	this->setStyleSheet(QString("background-color: %1").arg(QString::fromStdString(BlockGetColor(number))));
+	this->setStyleSheet(QString("background-color: %1").arg(QString::fromStdString(BlockGetColor(moveNumber))));
 	//palette的优先级小于setStyleSheet，只要父窗口设置了此函数，其他palette就会失效
 	this->setPalette(palette);
 }
 
-void Block::BlockSetFont(int number)
+void Block::BlockSetFont(int moveNumber)
 {
-	this->setText(QString::number(number));
+	this->setText(QString::number(moveNumber));
 	this->setFont(QFont("Arial", 30, 75));
 	this->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 }
 
-std::string Block::BlockGetColor(int number)
+std::string Block::BlockGetColor(int moveNumber)
 {
 	for (BlockColor bc : _colorMap) {
-		if (bc.getnumber() == number)
+		if (bc.getnumber() == moveNumber)
 			return bc.getcolor();
 	}
 }
--- QtCpp2048/core/Block.h
+++ QtCpp2048/core/Block.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <QLabel>
+#include <utility>
 #include <vector>
 class BlockColor
 {
 public:
-	BlockColor(int number, std::string color) : cnumber(number), ccolor(color) {
+	BlockColor(int number, std::string color) : cnumber(number), ccolor(std::move(color)) {
 	}
 	int getnumber() {
 		return cnumber;
@@ -26,15 +27,14 @@ public:
 
 	void SetBlock(int number);
 private:
-	int number = 0;
-	std::vector<BlockColor> _colorMap{ 
+	std::vector<BlockColor> _colorMap{
 		{BlockColor(2, "#eee4da")}, {BlockColor(4, "#ede0c8")}, {BlockColor(8, "#f2b179")},
 		{BlockColor(16, "#f59563")}, {BlockColor(32, "#f67c5f")}, {BlockColor(64, "#f65e3b")},
 		{BlockColor(128, "#edcf72")}, {BlockColor(256, "#edcc61")}, {BlockColor(512, "#edc850")},
 		{BlockColor(1024, "#edc53f")}, {BlockColor(2048, "#edc22e")}};
 
 private:
-	void BlockSetColor(int number);
+	void BlockSetColor(int moveNumber);
 	void BlockSetFont(int number);
 	std::string BlockGetColor(int number);
 };
